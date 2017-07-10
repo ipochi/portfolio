93fef0b4e13484f587b092fde7fd51d5eabccf0d
Imran Pochi <24697@ITCINFOTECH.COM>
2017-07-10 19:04:23 +0530

    About me Page , WIP

--- src/components/AboutMeComponent.vue
+++ src/components/AboutMeComponent.vue
@@ -1,12 +1,79 @@
 <template lang="html">
-  <div class="section">
-    <div class="container">
-      <div class="content is-large">
-        Hi , My name is Imran Pochi. I am developer
+  <div>
+    <section class="section">
+      <div class="columns">
+
+        <div class="column">
+
+        </div>
+        <div class="column is-5">
+          <div class="content has-text-right is-large">
+            <h1><strong>Hi , I am a software developer from Bangalore</strong></h1>
+          </div>
+        </div>
+        <div class="column is-5">
+
+          <div class="content has-text-centered has-padding">
+            <p class="title"> Versatile , Generalist</p>
+          </div>
+
+        </div>
+        <div class="column">
+
+        </div>
       </div>
-    </div>
-    <p>Hello Imran Pochi</p>
+    </section>
+
+      <div class="content is-large has-text-centered"><strong>I've got the chops in all phases of desgin process</strong></div>
+    <section class="section">
+      <div class="columns">
+        <div class="column">
+
+        </div>
+        <div class="column  has-text-centered">
+
+            <i class="fa fa-lightbulb-o fa-5x has-text-warning"></i>
+            <div class="content is-large"><strong>Ideas</strong></div>
+            <div class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              It has survived not only five centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged. It was popularised in the 1960s with the release of
+              Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of Lorem.
+            </div>
+        </div>
+        <div class="column  has-text-centered">
+            <i class="fa fa-magic fa-5x has-text-warning"></i>
+            <div class="content is-large"><strong>Magic Bullet</strong></div>
+            <div class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              It has survived not only five centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged. It was popularised in the 1960s with the release of
+              Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of Lorem.
+            </div>
+        </div>
+        <div class="column  has-text-centered">
+            <i class="fa fa-code fa-5x has-text-warning"></i>
+            <div class="content is-large"><strong>Code</strong></div>
+            <div class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              It has survived not only five centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged. It was popularised in the 1960s with the release of
+              Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of Lorem.
+            </div>
+        </div>
+        <div class="column">
+
+        </div>
+      </div>
+    </section>
   </div>
+
 </template>
 
 <script>
@@ -25,4 +92,7 @@ export default {
 
 <style lang="css">
 
+.has-padding {
+  padding-top: 12px;
+}
 </style>
