7cdd5d6626a33efa8e2d44f9c15650347dcfa3a7
Imran Pochi <24697@ITCINFOTECH.COM>
2017-07-13 18:07:17 +0530

    Added content for About and Work Pages , format and content is incomplete

--- src/components/AboutMeComponent.vue
+++ src/components/AboutMeComponent.vue
@@ -3,28 +3,36 @@
     <section class="section">
       <div class="columns">
 
-        <div class="column">
+        <div class="column is-2">
 
         </div>
-        <div class="column is-5">
+        <div class="column is-4">
           <div class="content has-text-right is-large">
-            <h1><strong>Hi , I am a software developer from Bangalore</strong></h1>
+            <h1><strong>Talented Software developer from Bangalore , India</strong></h1>
           </div>
         </div>
-        <div class="column is-5">
+        <div class="column is-4">
 
-          <div class="content has-text-centered has-padding">
-            <p class="title"> Versatile , Generalist</p>
+          <div class="content has-text-left has-padding-cs">
+            <p class="has-text-success"><strong> Versatile , Generalist</strong></p>
+            <p><strong> Passionate , Hardworking</strong></p>
+            <p><strong> Learner , Fighter</strong></p>
+            <p><strong> Learner , Fighter</strong></p>
           </div>
 
         </div>
-        <div class="column">
+        <div class="column is-2">
 
         </div>
       </div>
     </section>
 
-      <div class="content is-large has-text-centered"><strong>I've got the chops in all phases of desgin process</strong></div>
+    <div class="content is-large has-text-centered">
+      <strong>
+        I've got the chops in all phases of desgin process
+      </strong>
+    </div>
+
     <section class="section">
       <div class="columns">
         <div class="column">
@@ -32,7 +40,7 @@
         </div>
         <div class="column  has-text-centered">
 
-            <i class="fa fa-lightbulb-o fa-5x has-text-warning"></i>
+            <i class="fa fa-lightbulb-o fa-4x has-text-warning"></i>
             <div class="content is-large"><strong>Ideas</strong></div>
             <div class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
@@ -44,8 +52,8 @@
             </div>
         </div>
         <div class="column  has-text-centered">
-            <i class="fa fa-magic fa-5x has-text-warning"></i>
-            <div class="content is-large"><strong>Magic Bullet</strong></div>
+            <i class="fa fa-magic fa-4x has-text-warning"></i>
+            <div class="content is-large"><strong>Wand</strong></div>
             <div class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
               when an unknown printer took a galley of type and scrambled it to make a type specimen book.
@@ -56,7 +64,7 @@
             </div>
         </div>
         <div class="column  has-text-centered">
-            <i class="fa fa-code fa-5x has-text-warning"></i>
+            <i class="fa fa-code fa-4x has-text-warning"></i>
             <div class="content is-large"><strong>Code</strong></div>
             <div class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
@@ -66,6 +74,71 @@
               Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
               publishing software like Aldus PageMaker including versions of Lorem.
             </div>
+        </div>
+        <div class="column">
+
+        </div>
+      </div>
+    </section>
+
+    <div class="content is-large has-text-centered is-paddingless">
+      <strong>
+        Skillset
+      </strong>
+    </div>
+
+    <section class="section">
+      <div class="columns">
+        <div class="column">
+        </div>
+
+        <div class="column  has-text-centered">
+          <span class="subtitle">Getting better day by day ... </span>
+          <table class="table">
+            <tbody>
+              <tr>
+                <td>HTML / CSS</td>
+                <td class="has-text-right">Javascript</td>
+              </tr>
+              <tr>
+                <td>Java</td>
+                <td class="has-text-right">Go (Poison of choice)</td>
+              </tr>
+              <tr>
+                <td>Vue JS (Poison of choice)</td>
+                <td class="has-text-right">Responsive Design</td>
+              </tr>
+              <tr>
+                <td>Linux (Poison of choice)</td>
+                <td class="has-text-right">Git (Poison of choice)</td>
+              </tr>
+              <tr>
+                <td>Flex PLM</td>
+                <td class="has-text-right">Windchill</td>
+              </tr>
+            </tbody>
+          </table>
+          <span class="subtitle"> I know my way around ... </span>
+          <table class="table">
+            <tbody>
+              <tr>
+                <td>AWS</td>
+                <td class="has-text-right">Docker</td>
+              </tr>
+              <tr>
+                <td>Chef</td>
+                <td class="has-text-right">Go (Poison of choice)</td>
+              </tr>
+              <tr>
+                <td>React JS</td>
+                <td class="has-text-right">Webpack</td>
+              </tr>
+              <tr>
+                <td>Linux</td>
+                <td class="has-text-right">SQL / No SQL Databases</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
         <div class="column">
 
@@ -92,7 +165,11 @@ export default {
 
 <style lang="css">
 
-.has-padding {
-  padding-top: 12px;
+.has-padding-cs {
+  padding-top: 14px;
+}
+
+.no-margin {
+    margin: 0;
 }
 </style>
--- src/components/AboutWorkComponent.vue
+++ src/components/AboutWorkComponent.vue
@@ -1,6 +1,273 @@
 <template lang="html">
-  <div class="section">
-    <p>I work at ITC Infotech.</p>
+  <div>
+    <section class="section">
+      <div class="columns">
+
+        <div class="column is-2">
+
+        </div>
+
+        <div class="column is-4">
+
+          <div class="content has-text-centered">
+            <h1><strong>Work History</strong></h1>
+          </div>
+
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Implementation Consultant
+              </p>
+              <div class="card-header-icon">
+                <strong>ITC Infotech</strong>
+              </div>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                As a Flex PLM Developer for the client ( Global Brands Group ) , I customize FLEXPLM 10.1
+                application as per the requirements of the client.<br>
+
+                <br>I am also entrusted with the duties of SONAR management to keep the codebase above the mandatory parameters of compliance.
+                <br><a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+                <br>
+              </div>
+            </div>
+          </div>
+
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Software Engineering Analyst
+              </p>
+              <div class="card-header-icon">
+                <strong>Accenture</strong>
+              </div>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                As a Flex PLM Developer for the client ( Hasbro ) , I customized FLEXPLM 10.2
+                application as per the requirements of the client.<br>
+
+                <br>I also shortlisted candidates for hiring across the PLM Domain.
+
+                <br><a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+                <br>
+              </div>
+            </div>
+          </div>
+
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Software Intern
+              </p>
+              <div class="card-header-icon">
+              <strong>  eBay Inc.</strong>
+              </div>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                As a software intern , I was assigned a project to be 
+                <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+                <br>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        <div class="column is-4">
+
+          <div class="content has-text-centered">
+            <h1><strong>Awards</strong></h1>
+          </div>
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Implementation Consultant
+              </p>
+              <div class="card-header-icon">
+                <strong>ITC Infotech</strong>
+              </div>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+
+                <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+                <br>
+              </div>
+            </div>
+          </div>
+
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Software Engineering Analyst
+              </p>
+              <div class="card-header-icon">
+                <strong>Accenture</strong>
+              </div>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+                <br>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="column is-2">
+
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section">
+      <div class="content has-text-centered">
+        <h1><strong>Projects</strong></h1>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="columns">
+
+        <div class="column is-2">
+
+        </div>
+
+        <div class="column is-4">
+
+          <div class="content has-text-centered">
+            <h1><strong>At Work</strong></h1>
+          </div>
+
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Implementation Consultant
+              </p>
+              <div class="card-header-icon">
+                <strong>ITC Infotech</strong>
+              </div>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+                <br>
+              </div>
+            </div>
+          </div>
+
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Software Engineering Analyst
+              </p>
+              <div class="card-header-icon">
+                <strong>Accenture</strong>
+              </div>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+                <br>
+              </div>
+            </div>
+          </div>
+
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Software Intern
+              </p>
+              <div class="card-header-icon">
+              <strong>  eBay Inc.</strong>
+              </div>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+                <br>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        <div class="column is-4">
+
+          <div class="content has-text-centered">
+            <h1><strong>POCs and Fun</strong></h1>
+          </div>
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Implementation Consultant
+              </p>
+              <div class="card-header-icon">
+                <strong>ITC Infotech</strong>
+              </div>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+
+                <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+                <br>
+              </div>
+            </div>
+          </div>
+
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Software Engineering Analyst
+              </p>
+              <div class="card-header-icon">
+                <strong>Accenture</strong>
+              </div>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+                <br>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="column is-2">
+
+        </div>
+      </div>
+
+    </section>
   </div>
 </template>
 
